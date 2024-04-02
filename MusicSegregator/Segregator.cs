1c62ae3b8c6f44d91cb7469d5884be0136710258
marcelslom <95636743+marcelslom@users.noreply.github.com>
2024-04-02 23:23:53 +0200

    Handling unique filenames

--- MusicSegregator/Segregator.cs
+++ MusicSegregator/Segregator.cs
@@ -27,8 +27,9 @@ namespace MusicSegregator
                     var tags = mp3.Tag;
                     var filename = Path.GetFileName(sourceFile);
                     var destinationDir = GetDestinationDir(tags);
+                    var uniqueFilename = GetUniqueFilename(destinationDir, filename);
                     Directory.CreateDirectory(destinationDir);
-                    var destinationPath = Path.Combine(destinationDir, filename);
+                    var destinationPath = Path.Combine(destinationDir, uniqueFilename);
                     if (context.DeleteSourceFile)
                     {
                         System.IO.File.Move(sourceFile, destinationPath);
@@ -39,6 +40,26 @@ namespace MusicSegregator
                     }
                 }
             }
+        }
+
+        private string GetUniqueFilename(string dir, string originalFilename)
+        {
+            if (!System.IO.File.Exists(Path.Combine(dir, originalFilename)))
+            {
+                return originalFilename;
+            }
+            int counter = 0;
+            var filenameWithoutExtension = Path.GetFileNameWithoutExtension(originalFilename);
+            var extension = Path.GetExtension(originalFilename);
+
+            string uniqueFilename;
+            do
+            {
+                counter++;
+                uniqueFilename = $"{filenameWithoutExtension}_({counter}){extension}";
+            } while (System.IO.File.Exists(Path.Combine(dir, uniqueFilename)));
+
+            return uniqueFilename;
         }
 
         private string GetDestinationDir(Tag tags)

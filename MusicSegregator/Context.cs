269433992ad46cec049cca9bae6a92e0d460a9c7
marcelslom <95636743+marcelslom@users.noreply.github.com>
2024-04-02 21:13:59 +0200

    Context

---
+++ MusicSegregator/Context.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicSegregator
+{
+    internal class Context
+    {
+        private static readonly string DefaultDestinationFolderName = "music_segregated";
+        public static Context Instance { get; set; } = new Context();
+        private Context() { }
+
+        public string SourcePath { get; private set; }
+        public string DestinationPath { get; private set; }
+        public bool DeleteSourceFile { get;set; }
+        public string FilenameSchema { get; set; }
+        public bool RenameFile => !string.IsNullOrEmpty(FilenameSchema);
+        public bool CreateLogFiles { get; private set; }
+
+        public void LoadOptions(IOptions options)
+        {
+            if (string.IsNullOrWhiteSpace(options.SourcePath))
+            {
+                SourcePath = Environment.CurrentDirectory;
+            } 
+            else
+            {
+                SourcePath = options.SourcePath.Trim();
+            }
+
+            if (string.IsNullOrWhiteSpace(options.DestinationPath))
+            {
+                DestinationPath = Path.Combine(Environment.CurrentDirectory, DefaultDestinationFolderName);
+            }
+            else
+            {
+                DestinationPath = options.DestinationPath.Trim();
+            }
+
+            DeleteSourceFile = options.DeleteSourceFile;
+            FilenameSchema = options.FilenameSchema;
+            CreateLogFiles = options.CreateLogFiles;
+        }
+
+    }
+}
---
+++ MusicSegregator/IOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicSegregator
+{
+    internal interface IOptions
+    {
+        string SourcePath { get; }
+        string DestinationPath { get; }
+        bool DeleteSourceFile { get; }
+        string FilenameSchema { get; }
+        bool CreateLogFiles { get; }
+    }
+}
